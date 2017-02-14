ce64ef61def2b55a1fe09d5c306e040805154273
James Ye <jamye506@gmail.com>
2017-02-13 16:18:18 -0800

    assignment added

---
+++ WebFundamentals/Javascript/debugging.js
@@ -0,0 +1,27 @@
+// function slots(){
+//
+//   for(var i = 50; i > 0; i--){
+//     // console.log(Math.floor(Math.random()*100))
+//     if((Math.floor(Math.random()*100)) === 1){
+//       i = i + (Math.floor(Math.random()*100))
+//     }
+//     // return i;
+//   }
+//
+//   console.log("hello");
+// }
+//
+// console.log(slots());
+
+
+function hello(){
+  for(var i = 0; i<5;i++){
+    if(i === 3){
+      console.log("hello");
+    }
+    return i;
+    console.log("cool")
+  }
+}
+
+hello();
