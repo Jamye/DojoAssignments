194e138133850cbe1c5a4ae2c0d37b1ff5d5f993
James Ye <jamye506@gmail.com>
2017-02-16 14:25:25 -0800

    assignment added

---
+++ WebFundamentals/Javascript/i_do_declare.js
@@ -0,0 +1,27 @@
+var num1 = 232;
+var num2 = 2;
+var num3 = 89;
+var num4 = -4;
+
+var string1 ="Hello";
+var string2 ="Nice to";
+var string3 ="meet you";
+
+var truthy = true;
+var falsy = false;
+var unknown
+
+console.log("what'you're favorite number? " + num1);
+console.log(num2 + " cool for school");
+console.log(num3, num3);
+console.log(num4 + " Is a negative number");
+
+
+console.log(string1);
+console.log(string2);
+console.log(string3);
+
+
+console.log(truthy);
+console.log(falsy);
+console.log(unknown);
